ee626443f59b37edcd2f41a3358ef432a8fb523d
David Rose <david@drose.io>
2024-04-02 01:21:05 +0300

    logging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log('index.js file loaded');
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
