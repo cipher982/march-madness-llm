20345e44d9928403f9c82f4b2d3322ef72942d54
David Rose <david@drose.io>
2024-05-09 19:47:05 -0500

    more work towards compose deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 console.log('api.js file loaded');
 
+// log the backend ip
+console.log(`backend ip: ${process.env.REACT_APP_BACKEND_IP}`);
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || '/api',
+    baseURL: `http://${process.env.REACT_APP_BACKEND_IP}:8000/api`,
 });
 
 export default api;
